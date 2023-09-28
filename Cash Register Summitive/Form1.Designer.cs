7ccf4dd2e4d1c9b49e3328173e8730ffa02ceafe
Speedi1010 <dyladegr607@ed.amdsb.ca>
2023-09-28 10:30:05 -0400

    Add project files.

---
+++ Cash Register Summitive/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Cash_Register_Summitive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Calculate = new System.Windows.Forms.Button();
+            this.healingPotion = new System.Windows.Forms.Label();
+            this.manaPotion = new System.Windows.Forms.Label();
+            this.sleepPotion = new System.Windows.Forms.Label();
+            this.healingInput = new System.Windows.Forms.TextBox();
+            this.manaInput = new System.Windows.Forms.TextBox();
+            this.sleepInput = new System.Windows.Forms.TextBox();
+            this.subLabel = new System.Windows.Forms.Label();
+            this.untaxedLabel = new System.Windows.Forms.Label();
+            this.ta = new System.Windows.Forms.Label();
+            this.TaxLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tenderInput = new System.Windows.Forms.TextBox();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.ChangeButton = new System.Windows.Forms.Button();
+            this.Receiptlabel = new System.Windows.Forms.Label();
+            this.ReceiptButton = new System.Windows.Forms.Button();
+            this.printingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Calculate.Location = new System.Drawing.Point(45, 138);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(191, 32);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Calculate Total";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // healingPotion
+            // 
+            this.healingPotion.AutoSize = true;
+            this.healingPotion.Location = new System.Drawing.Point(45, 38);
+            this.healingPotion.Name = "healingPotion";
+            this.healingPotion.Size = new System.Drawing.Size(109, 16);
+            this.healingPotion.TabIndex = 1;
+            this.healingPotion.Text = "Potion of Healing";
+            // 
+            // manaPotion
+            // 
+            this.manaPotion.AutoSize = true;
+            this.manaPotion.Location = new System.Drawing.Point(45, 74);
+            this.manaPotion.Name = "manaPotion";
+            this.manaPotion.Size = new System.Drawing.Size(96, 16);
+            this.manaPotion.TabIndex = 2;
+            this.manaPotion.Text = "Potion of Mana";
+            // 
+            // sleepPotion
+            // 
+            this.sleepPotion.AutoSize = true;
+            this.sleepPotion.Location = new System.Drawing.Point(45, 108);
+            this.sleepPotion.Name = "sleepPotion";
+            this.sleepPotion.Size = new System.Drawing.Size(98, 16);
+            this.sleepPotion.TabIndex = 3;
+            this.sleepPotion.Text = "Potion of Sleep";
+            // 
+            // healingInput
+            // 
+            this.healingInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.healingInput.Location = new System.Drawing.Point(169, 38);
+            this.healingInput.Name = "healingInput";
+            this.healingInput.Size = new System.Drawing.Size(42, 15);
+            this.healingInput.TabIndex = 4;
+            this.healingInput.Text = "0";
+            // 
+            // manaInput
+            // 
+            this.manaInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.manaInput.Location = new System.Drawing.Point(169, 75);
+            this.manaInput.Name = "manaInput";
+            this.manaInput.Size = new System.Drawing.Size(42, 15);
+            this.manaInput.TabIndex = 5;
+            this.manaInput.Text = "0";
+            // 
+            // sleepInput
+            // 
+            this.sleepInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.sleepInput.Location = new System.Drawing.Point(169, 108);
+            this.sleepInput.Name = "sleepInput";
+            this.sleepInput.Size = new System.Drawing.Size(42, 15);
+            this.sleepInput.TabIndex = 6;
+            this.sleepInput.Text = "0";
+            // 
+            // subLabel
+            // 
+            this.subLabel.AutoSize = true;
+            this.subLabel.Location = new System.Drawing.Point(45, 183);
+            this.subLabel.Name = "subLabel";
+            this.subLabel.Size = new System.Drawing.Size(59, 16);
+            this.subLabel.TabIndex = 7;
+            this.subLabel.Text = "Subtotal;";
+            // 
+            // untaxedLabel
+            // 
+            this.untaxedLabel.AutoSize = true;
+            this.untaxedLabel.Location = new System.Drawing.Point(166, 183);
+            this.untaxedLabel.Name = "untaxedLabel";
+            this.untaxedLabel.Size = new System.Drawing.Size(0, 16);
+            this.untaxedLabel.TabIndex = 8;
+            // 
+            // ta
+            // 
+            this.ta.AutoSize = true;
+            this.ta.Location = new System.Drawing.Point(71, 199);
+            this.ta.Name = "ta";
+            this.ta.Size = new System.Drawing.Size(33, 16);
+            this.ta.TabIndex = 9;
+            this.ta.Text = "Tax:";
+            // 
+            // TaxLabel
+            // 
+            this.TaxLabel.AutoSize = true;
+            this.TaxLabel.Location = new System.Drawing.Point(166, 199);
+            this.TaxLabel.Name = "TaxLabel";
+            this.TaxLabel.Size = new System.Drawing.Size(0, 16);
+            this.TaxLabel.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Total:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(166, 215);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(0, 16);
+            this.totalLabel.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Tendered:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(26, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 18);
+            this.label3.TabIndex = 14;
+            // 
+            // tenderInput
+            // 
+            this.tenderInput.Location = new System.Drawing.Point(117, 263);
+            this.tenderInput.Name = "tenderInput";
+            this.tenderInput.Size = new System.Drawing.Size(39, 22);
+            this.tenderInput.TabIndex = 15;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Location = new System.Drawing.Point(47, 322);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(57, 16);
+            this.changeLabel.TabIndex = 16;
+            this.changeLabel.Text = "Change:";
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.AutoSize = true;
+            this.changeOutput.Location = new System.Drawing.Point(141, 322);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(0, 16);
+            this.changeOutput.TabIndex = 17;
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ChangeButton.Location = new System.Drawing.Point(29, 291);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.Size = new System.Drawing.Size(137, 23);
+            this.ChangeButton.TabIndex = 18;
+            this.ChangeButton.Text = "Calculate Change";
+            this.ChangeButton.UseVisualStyleBackColor = true;
+            this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // Receiptlabel
+            // 
+            this.Receiptlabel.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Receiptlabel.Location = new System.Drawing.Point(378, 38);
+            this.Receiptlabel.Name = "Receiptlabel";
+            this.Receiptlabel.Size = new System.Drawing.Size(401, 397);
+            this.Receiptlabel.TabIndex = 19;
+            // 
+            // ReceiptButton
+            // 
+            this.ReceiptButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ReceiptButton.Location = new System.Drawing.Point(29, 351);
+            this.ReceiptButton.Name = "ReceiptButton";
+            this.ReceiptButton.Size = new System.Drawing.Size(131, 29);
+            this.ReceiptButton.TabIndex = 20;
+            this.ReceiptButton.Text = "Print Receipt";
+            this.ReceiptButton.UseVisualStyleBackColor = true;
+            this.ReceiptButton.Click += new System.EventHandler(this.ReceiptButton_Click);
+            // 
+            // printingLabel
+            // 
+            this.printingLabel.Location = new System.Drawing.Point(378, 38);
+            this.printingLabel.Name = "printingLabel";
+            this.printingLabel.Size = new System.Drawing.Size(401, 397);
+            this.printingLabel.TabIndex = 21;
+            this.printingLabel.Click += new System.EventHandler(this.printingLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReceiptButton);
+            this.Controls.Add(this.ChangeButton);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.tenderInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TaxLabel);
+            this.Controls.Add(this.ta);
+            this.Controls.Add(this.untaxedLabel);
+            this.Controls.Add(this.subLabel);
+            this.Controls.Add(this.sleepInput);
+            this.Controls.Add(this.manaInput);
+            this.Controls.Add(this.healingInput);
+            this.Controls.Add(this.sleepPotion);
+            this.Controls.Add(this.manaPotion);
+            this.Controls.Add(this.healingPotion);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.printingLabel);
+            this.Controls.Add(this.Receiptlabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Potion Shop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label healingPotion;
+        private System.Windows.Forms.Label manaPotion;
+        private System.Windows.Forms.Label sleepPotion;
+        private System.Windows.Forms.TextBox healingInput;
+        private System.Windows.Forms.TextBox manaInput;
+        private System.Windows.Forms.TextBox sleepInput;
+        private System.Windows.Forms.Label subLabel;
+        private System.Windows.Forms.Label untaxedLabel;
+        private System.Windows.Forms.Label ta;
+        private System.Windows.Forms.Label TaxLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tenderInput;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Button ChangeButton;
+        private System.Windows.Forms.Label Receiptlabel;
+        private System.Windows.Forms.Button ReceiptButton;
+        private System.Windows.Forms.Label printingLabel;
+    }
+}
+
---
+++ Cash Register Summitive/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Cash_Register_Summitive
+{
+    public partial class Form1 : Form
+    {
+        int healingPotionPrice = 4;
+        int healingPotionamount;
+        int manaPotionPrice = 8;
+        int manaPotionamount;
+        int sleepPotionPrice = 10;
+        int sleeepPotionamount;
+        int subtotal;
+        double tax;
+        double total;
+        double change;
+        int number = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                healingPotionamount = Convert.ToInt16(healingInput.Text);
+                manaPotionamount = Convert.ToInt16(manaInput.Text);
+                sleeepPotionamount = Convert.ToInt16(sleepInput.Text);
+
+                subtotal = healingPotionamount * healingPotionPrice + manaPotionamount * manaPotionPrice + sleeepPotionamount * sleepPotionPrice;
+                tax = subtotal * .25;
+                total = subtotal + tax;
+
+                untaxedLabel.Text = $"{subtotal} Gold";
+                TaxLabel.Text = $"{tax} Gold";
+                totalLabel.Text = $"{total} Gold";
+            }
+            catch
+            {
+                untaxedLabel.Text = "пошел на хуй";
+                TaxLabel.Text = "пошел на хуй";
+                totalLabel.Text = "пошел на хуй";
+            }
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int tendered = Convert.ToInt16(tenderInput.Text);
+                change = tendered - total;
+                changeOutput.Text = $"{change} Gold";
+                if (tendered < 0)
+                {
+                    changeOutput.Text = "пошел на хуй";
+                }
+            }
+            catch
+            {
+                changeOutput.Text = "пошел на хуй";
+            }
+        }
+
+        private void ReceiptButton_Click(object sender, EventArgs e)
+        {
+            int location = 38;
+            int size = 380;
+            printingLabel.Size = new Size(401, size);
+            printingLabel.Location = new Point(280, location);
+            SoundPlayer receiptPrint = new SoundPlayer(Properties.Resources.receipt2);
+            if (change <= 0)
+            {
+                printingLabel.Size = new Size(0, 0);
+                Receiptlabel.Text = "You owe me money";
+                return;
+            }
+
+            if (total <= 0)
+            {
+                printingLabel.Size = new Size(0, 0);
+                Receiptlabel.Text = "Buy something you brokie";
+                return;
+            }
+            number++;
+
+            Receiptlabel.BackColor = Color.White;
+            Receiptlabel.Text = $"          The Devil's Potion Shop\n\n                 Receipt {number}";
+            Receiptlabel.Text += $"\n\nHealing Potions:   {healingPotionamount} for {healingPotionPrice} Gold each";
+            Receiptlabel.Text += $"\n\nMana Potions:      {manaPotionamount} for {manaPotionPrice} Gold each";
+            Receiptlabel.Text += $"\n\nSleep Potions:     {sleeepPotionamount} for {sleepPotionPrice} Gold each";
+            Receiptlabel.Text += $"\n\nSubtotal:          {subtotal} Gold";
+            Receiptlabel.Text += $"\n\nTax:               {tax} Gold";
+            Receiptlabel.Text += $"\n\nTotal:             {total} Gold";
+            Receiptlabel.Text += $"\n\nTendered:          {tenderInput.Text} Gold";
+            Receiptlabel.Text += $"\n\nChange:            {change} Gold";
+            Receiptlabel.Text += $"\n\n\n\n   Have a nice day come by again";
+
+            for (int x = 0; x < 19; x++)
+            {
+                size = size - 17;
+                location = location + 17;
+
+                receiptPrint.Play();
+                Refresh();
+                Thread.Sleep(1000);
+
+                printingLabel.Size = new Size(401, size);
+                printingLabel.Location = new Point(280, location);
+            }
+        }
+
+        private void printingLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
